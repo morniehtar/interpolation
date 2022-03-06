ca59757b8be33d3333f373b224f18746fc035083
Alexander <46871606+morniehtar@users.noreply.github.com>
2022-03-06 16:21:11 +0300

    incapsulated interpolation into a separate function

--- main.f90
+++ main.f90
@@ -1,72 +1,75 @@
-function interp(x, n, arg, func) result(res)
+#define _pr 8
+
+module config
+    implicit none
+    public
+    !Границы отрезка интерполяции
+    real(_pr), parameter :: min = 0, max = 10
+    !Точность интерполяции
+    integer, parameter :: intPrec=18
+    !Количество выводимых точек полинома
+    integer, parameter :: dots = 100
+end module
+
+program draw
+    use config
     implicit none
 
-    real(4), dimension(*), intent(in) :: arg, func
-    real(4), intent(in) :: x
-    integer, intent(in) :: n
+    integer :: i
+    real(_pr) :: x
+    procedure(func), pointer :: fptr
 
-    real(4) :: res, store
-    integer :: i, j
+    fptr => func
 
-    if ((x-arg(1))<0. .or. (x-arg(n))>0.) stop
-
-    res=0
-    do i = 1, n
-        store = 1
-        do j = 1, n
-            if (j/=i) then
-                store=store*(x-arg(j))/(arg(i)-arg(j))
-            end if
-        end do
-        res=res+(func(i)*store)
-    end do
-end function
-
-program Lagrange
-    implicit none
-
-    integer :: n, i, m
-    real(4), dimension(:), allocatable :: arg
-    real(4), dimension(:), allocatable :: func
-    real(4) :: min, max, x
-
-    interface
-        real(4) function interp(x, n, arg, func)
-            real(4), dimension(*), intent(in) :: arg, func
-            integer, intent(in) :: n
-            real(4), intent(in) :: x
-        end function interp
-    end interface
-
-    !Configuration
-    n = 18  !Number of known points
-    m = 100 !Points after interpolation 
-    min = -1.
-    max =  1.
-
-    allocate(arg(n))
-    allocate(func(n))
-
-    !Choose n points at regular intervals of lenghth del
-    do i = 1, n
-        arg(i)=min+(i-1)*((max-min)/(n-1))
-    end do
-
-    !Write the values of the function on those points into an array
-    do i = 1, n
-        !func(i)=sin(arg(i))
-        func(i)=1/(25*arg(i)**2+1)
-    end do
-
-    !Interpolation itself
-    open (unit = 1, file = "output.dat")
-    do i = 1, m
-        x=min+(i-1)*((max-min)/(m-1))
-        !write(1,*) x, interp(x, n, arg, func), sin(x)
-        write(1,*) x, interp(x, n, arg, func), 1/(25*x**2+1)
+    open(unit = 1, file = "output.dat")
+    do i = 1, dots
+        x = min + (i-1)*((max-min)/(dots-1))
+        write(1,*) x, lagr(fptr, x, min, max, intPrec), func(x)
     end do
     close(1)
 
-    deallocate(arg)
-    deallocate(func)
-end program
+contains
+    function func(arg) result(res)
+        implicit none
+
+        real(_pr), intent(in) :: arg
+        real(_pr) :: res
+
+        res = sin(arg)
+    end function func
+
+    function lagr(fptr, arg, min, max, prec) result(res)
+        implicit none
+
+        procedure(func), pointer, intent(in) :: fptr
+        real(_pr), intent(in) :: arg, min, max
+        integer, intent(in) :: prec
+
+        real(_pr) :: res, store
+        real(_pr), dimension(:), allocatable :: arrg
+        integer :: i, j
+
+        if ((arg-min)<0. .or. (arg-max)>0.) stop
+
+        allocate(arrg(prec))
+
+        do i = 1, prec
+            arrg(i) = min + (max-min)*(i-1)/(prec-1)
+        end do
+
+        res=0
+        do i = 1, prec
+            store = 1
+            do j = 1, prec
+                if (j/=i) then
+                    store=store*(arg-arrg(j))/(arrg(i)-arrg(j))
+                end if
+            end do
+            res=res+(fptr(arrg(i))*store)
+        end do
+
+        deallocate(arrg)
+    end function lagr
+
+
+end program draw
